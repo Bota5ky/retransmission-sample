30335063c7166aa0e79730ccca12c4541ee361e4
Bota5ky <zjy2516@126.com>
2023-08-31 16:42:20 +0800

    remove unused imports

--- mvc/src/main/java/com/thoughtworks/controller/RemoteClient.java
+++ mvc/src/main/java/com/thoughtworks/controller/RemoteClient.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.Map;
